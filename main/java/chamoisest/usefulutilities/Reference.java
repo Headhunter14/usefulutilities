ab4161f6bb9814cdc1339ccc0f99a8c7ea619b4a
Headhunter14 <ivarakkeolenma@gmail.com>
2017-01-19 23:55:15 +0200

    Updated version number

--- main/java/chamoisest/usefulutilities/Reference.java
+++ main/java/chamoisest/usefulutilities/Reference.java
@@ -3,7 +3,7 @@ package chamoisest.usefulutilities;
 public class Reference {
 
 	public static final String MODID = "usefulutilities";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String NAME = "Useful Utilities";
 	public static final String SERVER_PROXY_CLASS = "chamoisest.usefulutilities.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "chamoisest.usefulutilities.proxy.ClientProxy";
